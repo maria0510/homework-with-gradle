6bc956e5d43210158515bf515bde3cdc939266f3
Dmitriy Butakov <dmitry404@gmail.com>
2016-10-22 18:01:43 +0300

    Removing an example

--- homework/lesson0.1/this_will_not_be_run/src/main/java/Library.java
+++
@@ -1,7 +0,0 @@
-public class Library {
-    public boolean someLibraryMethod(boolean condition) {
-      System.out.println("return condition");
-
-      return condition;
-    }
-}
--- homework/lesson0/solutions/jane.roe/src/main/java/jelementary/Calculator.java
+++
@@ -1,7 +0,0 @@
-package jelementary;
-
-public class Calculator {
-  public int run(String expr) {
-    return Integer.valueOf(expr);
-  }
-}
