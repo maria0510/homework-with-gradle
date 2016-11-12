d0c1f386d31d6d3506493ba6c1c83d763aea704b
andreikramchaninov <andreikramchaninov@gmail.com>
2016-11-12 08:12:35 +0200

    Andrei Kramchaninov: homework 9 (#19)

---
+++ homework/lesson_9/andrey.kramchaninov/src/main/java/Main.java
@@ -0,0 +1,15 @@
+import exersice.GrandParentClass;
+import exersice.ParentClass;
+import exersice.TopLevelClass;
+import exersice.logger.InitOrderLogger;
+
+class Main {
+
+  public static void main(String ...args) {
+    GrandParentClass gpc = new GrandParentClass();
+    ParentClass pc = new ParentClass();
+    TopLevelClass tlc = new TopLevelClass();
+
+  }
+
+}
